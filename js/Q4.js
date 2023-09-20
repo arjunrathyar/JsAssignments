9e1a7d60698699e685c37a6a0fef260df3d21653
Arjun Rathya R <arjunrathyar@am.students.amrita.edu>
2023-09-20 09:19:36 +0530

    Add files via upload

---
+++ js/Q4.js
@@ -0,0 +1,7 @@
+function showHide(element) {
+    if (element == "Other") {
+        document.getElementById('otherText').style.display = "block";
+    } else {
+        document.getElementById('otherText').style.display = "none";
+    }
+}
