00d354f1a77716f6c19fc81f9d140ef39dab61c1
Arjun Rathya R <arjunrathyar@am.students.amrita.edu>
2023-09-20 23:24:43 +0530

    Delete js directory

--- js/Q4.js
+++
@@ -1,7 +0,0 @@
-function showHide(element) {
-    if (element == "Other") {
-        document.getElementById('otherText').style.display = "block";
-    } else {
-        document.getElementById('otherText').style.display = "none";
-    }
-}
