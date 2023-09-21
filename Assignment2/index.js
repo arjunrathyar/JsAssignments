a0070dd95530d984c0af0f80cd9d8928582b8966
Arjun Rathya R <arjunrathyar@am.students.amrita.edu>
2023-09-21 21:29:46 +0530

    Add files via upload

--- Assignment2/index.js
+++ Assignment2/index.js
@@ -1,14 +1,40 @@
 import { Astronaut } from "./Astronaut.js";
 import { Cargo } from "./Cargo.js";
 import { Rocket } from "./Rocket.js";
-let rocket = new Rocket('Rocket1', 100000);
-let astronaut = new Astronaut('Neil', 80);
-let cargo = new Cargo('Missile', 800000);
-let cargo2 = new Cargo('Food', 8000);
-let cargo3 = new Cargo('Luggage', 9800);
-console.log(rocket.addAstronaut(astronaut));
-console.log(rocket.addCargo(cargo));
-console.log(rocket.addCargo(cargo2));
-console.log(rocket.addCargo(cargo3));
-console.log(rocket.cargoItems);
-console.log(rocket.astronauts);
+var rocket;
+var flag = 0;
+let choice = 'y';
+while (choice == 'y') {
+    var promptChoice = prompt("Enter your Choice: \n1. Add Rocket \n2. Add Cargo \n3. Add Astronaut \n4. Display through Console \n5. Exit");
+    switch (promptChoice) { //Menu driven prompt
+        case "1":
+            if (flag == 0) {
+                let rocketName = prompt("Enter Rocket Name: ");
+                let rockettCapacity = prompt("Enter Rocket Capacity: ");
+                rocket = new Rocket(rocketName, parseInt(rockettCapacity)); //Multiple rockets can be handled using a list for rockets and collecting them based on an id
+                flag = 1;
+            }
+            else {
+                alert("Only one rocket can be handled!!");
+            }
+            break;
+        case "2":
+            let CargoName = prompt("Enter Cargo Name: ");
+            let CargoWeight = prompt("Enter Cargo Weight: ");
+            rocket.addCargo(new Cargo(CargoName, parseInt(CargoWeight)));
+            break;
+        case "3":
+            let AstronautName = prompt("Enter Astronaut Name: ");
+            let AstronautWeight = prompt("Enter Astronaut Weight: ");
+            rocket.addAstronaut(new Astronaut(AstronautName, parseInt(AstronautWeight)));
+            break;
+        case "4":
+            console.log(rocket.cargoItems);
+            console.log(rocket.astronauts);
+            break;
+        case "5":
+            choice = 'n';
+        default:
+            break;
+    }
+}
